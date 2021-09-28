14c30827cff697b1b7535230a8b8bb6e9499abe7
p22dhealy <p22dhealy@fairfieldprep.org>
2021-09-28 10:06:50 -0400

    Submit lab 2

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# author DMH 9/28/21
+
+number = input("Please Input a Number:")
+
+if int(number) % 2 == 0:
+    print(number + " is an even number.")
---
+++ lab_2-2.py
@@ -0,0 +1,10 @@
+# author DMH 9/28/21
+
+number = input("Please Input a Number:")
+
+if int(number) % 2 == 0:
+    print(number + " is an even number.")
+else: 
+    print(number + " is an odd number.")
+
+ 
